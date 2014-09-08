45d3330bfcdb884fe7e9f91cf34ccf9244866fd5
Dion Koolhaas <dionkoolhaas@MacBook-Pro-van-Dion.local>
2014-09-08 15:26:31 +0200

    no platforms

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/dionkoolhaas/autobetesBeta/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/dionkoolhaas/autobetesBeta/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/dionkoolhaas/autobetesBeta/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
