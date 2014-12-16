3a75d5958ecd1574d8d385d4eb8e0fed3b9dbdb7
mdijkstra <mdijkstrasEmailAddressIsHidden>
2014-12-16 14:35:32 +0100

    fix

--- install_plugins.sh
+++ install_plugins.sh
@@ -2,4 +2,4 @@ phonegap plugin add org.apache.cordova.statusbar
 phonegap local plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
 phonegap local plugin add org.apache.cordova.inappbrowser
 phonegap local plugin add org.apache.cordova.console
-cordova local plugin add org.apache.cordova.network-information
+cordova plugin add org.apache.cordova.network-information
